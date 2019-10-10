ce1a1a65792d77e95c3e3c1e562e6df561aeffe0
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-10 17:23:10 -0400

    Update versioncheck.js
    will add iOS 12 support for pwnedOS11

--- versioncheck.js
+++ versioncheck.js
@@ -1,9 +1,9 @@
 if(
-    !/\b11_\S+ like Mac OS X/.test(navigator.userAgent)
+    !/\b11_\S+ like Mac OS X/.test(navigator.userAgent) || !/\b12_\S+ like Mac OS X/.test(navigator.userAgent)
 )
 {
 
 window.alert("Error, Unsupported Firmware");
-window.alert("Sorry, pwnedOS11 only supports iOS 11 - 11.3.1");
+window.alert("Sorry, pwnedOS11 only supports iOS 11 - 12");
 return -1;
 }
