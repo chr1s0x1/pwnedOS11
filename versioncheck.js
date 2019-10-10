b22a023b81213b41aa07f2459b3ac10a54c732de
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-10 12:53:59 -0400

    Update versioncheck.js
    not needed

--- versioncheck.js
+++ versioncheck.js
@@ -3,7 +3,7 @@ if(
 )
 {
 
-window.alert("Error, Unsupported Firmware\n");
+window.alert("Error, Unsupported Firmware");
 window.alert("Sorry, pwnedOS11 only supports iOS 11 - 11.3.1");
 return -1;
 }
