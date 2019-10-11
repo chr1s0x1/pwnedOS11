22b20b3d853928c2f000c43e2415626bdd53eeae
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-11 11:15:36 -0400

    Delete versioncheck.js
    in home folder

--- versioncheck.js
+++
@@ -1,9 +0,0 @@
-if(
-    !/\b11_\S+ like Mac OS X/.test(navigator.userAgent) || !/\b12_\S+ like Mac OS X/.test(navigator.userAgent)
-)
-{
-
-window.alert("Error, Unsupported Firmware");
-window.alert("Sorry, pwnedOS11 only supports iOS 11 - 12");
-return -1;
-}
