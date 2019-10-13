993a3ead21b1b6e9233dd2103c2f2c1257786ac9
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-13 16:40:45 -0400

    go.js
    change some things that's not needed

--- Home/js/go.js
+++ Home/js/go.js
@@ -2,11 +2,10 @@
 // Get user agent to determine what exploit to use...
 // WIP
              function uAgent() {
-                 var getAgent = navigation.userAgent;
                  
-                 if (getAgent <= 12.4)
+                 if (/\b12_\S+ like Mac OS X/.test(navigator.userAgent))
                      document.body.innerHTML = "<h4>Device is on iOS 12\n will use SockPuppet 3..</h4>";
-             }else if (getAgent <= 11.3) {
+             }else if (/\b11_\S+ like Mac OS X/.test(navigator.userAgent)) {
                  // Saying 11.3.1 draws an error
                  document.body.innerHTML = "<h4>Device is on iOS 11\n will use multi_path..</h4>";
              }else {
