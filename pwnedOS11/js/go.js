79d73abf9df725052166be4c0aab05f31480bc3d
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-13 21:30:55 -0400

    Add files via upload
    reorganize GitHub repo - add execution of iOS 11 safari exploit

---
+++ pwnedOS11/js/go.js
@@ -0,0 +1,18 @@
+
+// Get user agent to determine what exploit to use...
+// WIP
+             function uAgent() {
+                 // Will store iOS version (if ios 11 or 12)
+                 var exploitByOS;
+                 
+                 if (/\b12_\S+ like Mac OS X/.test(navigator.userAgent))
+                     document.body.innerHTML = "<h4>Device is on iOS 12\n will use SockPuppet 3..</h4>";
+                 exploitByOS = 12;
+             }else if (/\b11_\S+ like Mac OS X/.test(navigator.userAgent)) {
+                 // Saying 11.3.1 draws an error
+                 document.body.innerHTML = "<h4>Device is on iOS 11\n will use multi_path..</h4>";
+                 exploitByOS = 11;
+             }else {
+                 alert("Error, iOS version unknown, Quitting..");
+                 location.reload();
+             }
