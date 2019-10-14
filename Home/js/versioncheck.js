bd94c3a55c8396e2c306742403e685514f3f745b
Chr1sPwned <chr1spwn3d@gmail.com>
2019-10-13 21:46:52 -0400

    remove copies
    put everything so far in 1 folder

--- Home/js/versioncheck.js
+++
@@ -1,9 +0,0 @@
-
-// Credit to Sem Voigtländer (@userlandkernel) for finding iOS Version
-// Get iOS Version
-function detectOSVersion() {
-    if (!/\b11_\S+ like Mac OS X/.test(navigator.userAgent) || !/\b12_\S+ like Mac OS X/.test(navigator.userAgent))
-window.alert("Error, Unsupported Firmware");
-window.alert("Sorry, pwnedOS11 only supports iOS 11 - 12.4");
-return -1;
-}
