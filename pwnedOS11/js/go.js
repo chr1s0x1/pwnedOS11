6279731c9cad5bce64d0381f7cf109d2b52fc44d
Chr1sPwned <chr1spwn3d@gmail.com>
2019-10-14 20:23:08 -0400

    ok

--- pwnedOS11/js/go.js
+++ pwnedOS11/js/go.js
@@ -6,11 +6,11 @@
                  var exploitByOS;
                  
                  if (/\b12_\S+ like Mac OS X/.test(navigator.userAgent))
-                     document.body.innerHTML = "<h4>Device is on iOS 12\n will use SockPuppet 3..</h4>";
+                     document.body.innerHTML = "<h4>Device is on iOS 12..</h4>";
                  exploitByOS = 12;
              }else if (/\b11_\S+ like Mac OS X/.test(navigator.userAgent)) {
                  // Saying 11.3.1 draws an error
-                 document.body.innerHTML = "<h4>Device is on iOS 11\n will use multi_path..</h4>";
+                 document.body.innerHTML = "<h4>Device is on iOS 11..</h4>";
                  exploitByOS = 11;
              }else {
                  alert("Error, iOS version unknown, Quitting..");
