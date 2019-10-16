83cb8b7201fb80b109db205e34c6ad9aea58f43f
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2019-10-16 15:59:48 -0400

    Actual working version check
    Credit to turnerrocks1 for adding a working version check

--- js/versioncheck.js
+++ js/versioncheck.js
@@ -1,7 +1,37 @@
 
 // Get iOS Version
+// Credit to turnerrocks1 (@turnerhackz1) for the iOSVersion() & detectBuild() functions (and separating the unsupported 
+// alerts into a function)
 function detectOSVersion() {
-    if (!/\b11_\S+ like Mac OS X/.test(navigator.userAgent) || !/\b12_\S+ like Mac OS X/.test(navigator.userAgent))
+function unsupported() {
 window.alert("Error, Unsupported Firmware");
 window.alert("Sorry, pwnedOS11 only supports iOS 11 - 12.1.1");
+}
+    function iOSVersion() {
+    var match = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),
+        version;
+
+    if (match !== undefined && match !== null) {
+        version = [
+            parseInt(match[1], 10),
+            parseInt(match[2], 10),
+            parseInt(match[3] || 0, 10)
+        ];
+        return parseFloat(version.join('.'));
+    }
+    return false;
+}
+//Algorithm for detecting the iOS Firmware build version using the userAgent
+function detectBuild() {
+    var tmp = navigator.userAgent.indexOf('Mobile/') > -1;
+    if(tmp) {
+        tmp = navigator.userAgent.split('Mobile/');
+        if(tmp.length<1) return null;
+        return alert(tmp[1].split(' ')[0]);
+    }
+    return null;
+}
+if (iOSVersion() != '12.1' || '12.0' || '12.1.2' || '12.1.4') {
+    break;
+} else {unsupported();}
 }
