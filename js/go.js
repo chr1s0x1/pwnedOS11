4428b453e2bfb29ac24a5328b0301809d8c0c368
Chr1sPwn3d <42826812+Chr1sPwned@users.noreply.github.com>
2020-02-19 21:08:24 -0500

    Update go.js
    don't need this since it's already in "index.html"

--- js/go.js
+++ js/go.js
@@ -2,16 +2,15 @@
 // Determine what exploit to use via iOSVersion() in versioncheck.js..
 // WIP, can be improved on.
              function DetermineExploit() {
+               
                  // Will store iOS version (if ios 11 or 12)
                  var exploitByOS;
                  
                  if (iOSVersion() > '11')
-                     document.body.innerHTML = "<h4>Device is on iOS 11..</h4>";
                  exploitByOS = 12;
              }else if (iOSVersion() < '12') {
-                 document.body.innerHTML = "<h4>Device is on iOS 12..</h4>";
                  exploitByOS = 11;
              }else {
-                 alert("Error, iOS version unknown, Quitting..");
+                 alert("error : go.js");
                  location.reload();
              }
