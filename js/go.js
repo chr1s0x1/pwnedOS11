fd09fe74fedaeb8119a3a7e57c56a8505c23978e
chr1s0x1 <chr1spwn3d@gmail.com>
2020-02-20 22:44:31 -0500

    welp, even more curly braces forgotten to add and some changes in some code

--- js/go.js
+++ js/go.js
@@ -2,27 +2,31 @@
 // Determine what exploit to use via iOSVersion() in versioncheck.js..
 // WIP, can be improved on.
 
-function iOSVersion() {
-    var match = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),
-        version;
 
-    if (match !== undefined && match !== null) {
-        version = [
-            parseInt(match[1], 10),
-            parseInt(match[2], 10),
-            parseInt(match[3] || 0, 10)
-        ];
-        return parseFloat(version.join('.'));
-    }
-    return false;
-}
 
              function determineExploit() {
+                 
+             document.body.innerHTML = "<h4><center>Getting iOS version..</center></h4>";
+                 
+                 function iOSVersion() {
+                     var match = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),
+                         version;
+
+                     if (match !== undefined && match !== null) {
+                         version = [
+                             parseInt(match[1], 10),
+                             parseInt(match[2], 10),
+                             parseInt(match[3] || 0, 10)
+                         ];
+                         return parseFloat(version.join('.'));
+                     }
+                     return false;
+                 }
                
                  // Will store iOS version (if ios 11 or 12)
                  var exploitByOS;
                  
-                 if (iOSVersion() > '11')
+                 if (iOSVersion() > '11') {
                  exploitByOS = 12;
              }else if (iOSVersion() < '12') {
                  exploitByOS = 11;
