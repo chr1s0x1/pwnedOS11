d6ec1f5b9e97d7df68b9d5f4f6c03a4bc85529db
chr1s0x1 <chr1spwn3d@gmail.com>
2020-02-29 01:06:34 -0500

    k

--- js/go.js
+++ js/go.js
@@ -11,20 +11,6 @@
                  
              document.body.innerHTML = "<h4><center>Getting iOS version..</center></h4>";
      
-    function iOSVersion() {
-       var match = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/),
-           version;
-
-       if (match !== undefined && match !== null) {
-           version = [
-            parseInt(match[1], 10),
-            parseInt(match[2], 10),
-            parseInt(match[3] || 0, 10)
-        ];
-        return parseFloat(version.join('.'));
-    }
-    return false;
-}
                  // Will store iOS version (if ios 11 or 12)
                  var exploitByOS = 0;
                  let ver = iOSVersion();
